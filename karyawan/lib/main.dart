e711a3f4feaa95b84a1e3241379be563c960573c
Your Name <you@example.com>
2026-03-04 12:54:07 +0700

    MEMBUAT PROJECT BARU"PILEM"
    asda

--- karyawan/lib/main.dart
+++ karyawan/lib/main.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'models/karyawan.dart';
-import 'dart:convert';
+import 'package:karyawan/models/karyawan.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Daftar Karyawan',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: HomePage()
@@ -25,23 +26,49 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  Future<List<Karyawan>> loadKaryawan() async {
-    final String response = await rootBundle.loadString("assets/karyawan.json");
-    final List<dynamic> data = json.decode(response);
-    return Karyawan.fromJsonList(data);
+  Future<List<Karyawan>> loadKaryawan() async{
+    final String reponse = await rootBundle.loadString("assets/karyawan.json");
+    final List<dynamic> data =  json.decode(reponse);
+    return data.map((json) => Karyawan.fromJson(json)).toList();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text("Daftar Karyawan"),
-      backgroundColor: Colors.lightBlueAccent,
-    ),
-    body: FutureBuilder<List<Karyawan>>(
-      
-    )
+      appBar: AppBar(
+        title: Text("Daftar Karyawan"),
+      ),
+      body: FutureBuilder(
+        future: loadKaryawan(), 
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final karyawan = snapshot.data![index];
+                return ListTile(
+                  title: Text(karyawan.nama,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Umur : ${karyawan.umur}"),
+                      Text("Alamat : ${karyawan.alamat.jalan}, "
+                                    "${karyawan.alamat.kota}, "
+                                    "${karyawan.alamat.provinsi}"),
+                      Text("Hobi : ${karyawan.hobi.join(", ")}"),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text("Error: ${snapshot.error}"));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        }),
     );
   }
 }
