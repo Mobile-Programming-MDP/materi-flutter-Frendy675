d0ce816a54f18a4fcb5ef07c6956cf06bee476b7
Frendy675 <frendyw7123@gmail.com>
2026-04-02 13:06:25 +0700

    lanjutan dari apk cepu

--- cepu/cepu_app/lib/main.dart
+++ cepu/cepu_app/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:cepu_app/firebase_options.dart';
-import 'package:cepu_app/screens/home_screen.dart';
-import 'package:cepu_app/screens/sign_in_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cepu_app/screens/sign_in_screen.dart';
+import 'package:cepu_app/screens/sign_up_screen.dart';
+import 'package:cepu_app/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,7 +21,6 @@ class MyApp extends StatelessWidget {
       title: "Cepu App",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
       ),
       home: StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
